56f51e0a9aaae7a3bc19c422c833387137edfd67
EC2 Default User <ec2-user@ip-172-31-8-124.ap-south-1.compute.internal>
2026-09-09 18:34:10 +0000

    Add Terraform EC2 infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "devops_ec2" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "aws-devops-terraform"
+  }
+}
