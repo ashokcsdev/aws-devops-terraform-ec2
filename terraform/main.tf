086346986cb79bf9a3da21613fd5f2066237ab92
ashokcsdev <ashokcs.dev@gmail.com>
2026-09-14 15:38:32 +0530

    Manage EC2 security group with Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,7 @@ terraform {
     region  = "ap-south-1"
     encrypt = true
   }
-}
-terraform {
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
@@ -18,9 +17,48 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "devops_sg" {
+  name        = "aws-devops-sg"
+  description = "Security group for AWS DevOps application"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Spring Boot application"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "aws-devops-sg"
+  }
+}
+
 resource "aws_instance" "devops_ec2" {
-  ami           = "ami-0f58b397bc5c1f2e8"
-  instance_type = "t3.micro"
+  ami                    = "ami-0f58b397bc5c1f2e8"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
 
   tags = {
     Name = "aws-devops-terraform"
